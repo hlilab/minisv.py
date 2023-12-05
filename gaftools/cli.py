3e962f78ca3a2a2e2687f04fe08e936aabbc3d98
qinqian <qqin@broadinstitute.org>
2023-12-05 04:20:47 +0000

    clean up codes and readme

--- gaftools/cli.py
+++ gaftools/cli.py
@@ -6,7 +6,7 @@ from .gaftools import GafParser
 import gzip
 
 
-@click.group(help="Fusion Benchmarking analysis commands")
+@click.group(help="Pangenome SV tool commands")
 def cli():
     pass
 
@@ -40,14 +40,3 @@ def parse(input: str, support_read:int,
     gaf = GafParser(input, prefix)
     gaf.parse_indel(mapq, mlen)
     gaf.merge_indel(min_cnt=support_read, min_mapq=mapq)
-
-    # output = gzip.open(f"{prefix}.bed.gz", 'wt')
-
-    # for read_blocks in gaf.parse_gaf():
-    #     for del_read_seg in read_blocks:
-    #         largedel_coords_dict[del_read_seg] += 1
-    # del_num = 0
-    # for key, value in largedel_coords_dict.items():
-    #     if value >= support_read:
-    #         output.write(f"{key[0]}\t{key[1]}\t{key[2]}\tlargedel{del_num}\t{value}\t{key[3]}\n")
-    #         del_num += 1
--- gaftools/gaftools.py
+++ gaftools/gaftools.py
@@ -6,11 +6,6 @@ import re
 import gzip
 import math
 
-CODE2CIGAR = "MIDNSHP=XB"
-CIGAR2CODE = dict((y, x) for x, y in enumerate(CODE2CIGAR))
-CIGAR_REGEX = re.compile("(\d+)([MIDNSHP=XB])")
-CHROM_PATTERN = r"^chr[0-9XY]{1,2}$"
-
 
 class GafParser(object):
     """Gaf file parser 
@@ -176,101 +171,6 @@ class AlignedRead(object):
     def path_end(self):
         return int(self.read[8])
 
-    def extract_graph_path_exact_coordinate(self, parts:list, min_del_len:int = 50) -> list:
-        """ Calculate exact genome coordinate by converting graph path string, start and end 
-        into a bed-like coordinate for a given reference
-
-        We focus on simple deletion example with both reads landing on the reference segments:
-        path:'>chr1:460900-479179>HG02109#1#JAHEPG010000279.1:461634-461770>chr1:479192-494904'
-        start: 795
-        end: 25349
-        output: chr1\t460900+795+CIGAR_deletion_start\t460900+795+CIGAR_deletion_end
-
-        We ignore complicated case for the future development.
-        path:'<HG01891#1#JAGYVO010000066.1:9329914-9343284>HG02055#1#JAHEPK010000072.1:70420115-70420122<HG01891#1#JAGYVO010000066.1:9307976-9325196'
-        start:4523    
-        end:23231
-        output:?
-        """
-        segs_major_reference_chrom = [seg.split(':')[0] for seg in re.split(r'[><]', self.path) if seg != '']
-        # some CHM13 mapped to GRCH38 such as 'GRCh38#0#chr1', we ignore such a case now
-        segs_major_reference_chrom = [chrom for chrom in segs_major_reference_chrom if re.match(CHROM_PATTERN, chrom)]
-        if len(segs_major_reference_chrom) == 0:
-            return []
-        assert len(set(segs_major_reference_chrom)) == 1, 'only one major chrom expected after removing non-chrom segments'
-
-        del_blocks = []
-        # graph path start and end are relative coordinate
-        ref_pos = self.path_start
-        for length, op in parts:
-            length = int(length)
-            if op == 'D' and length >= min_del_len:
-                del_blocks.append((self.path, ref_pos, ref_pos + length, self.strand))
-            if op in ["M", "D", "N", "=", "X"]:
-                ref_pos += length
-
-        # convert major chromosome position to relative positions
-        path_segment_list = [seg for seg in re.split(r'[><]', self.path) if seg != '']
-        output_exact_coordiantes = []
-        path_seg_cumulative_length = 0 
-        for seg_interval in path_segment_list:
-            seg_interval = seg_interval.split(':')
-            seg_chrom = seg_interval[0]
-            seg_start, seg_end = map(int, seg_interval[1].split('-'))
-            # 0-based coordinate 
-            # https://github.com/lh3/gfatools/blob/master/doc/rGFA.md
-            path_seg_cumulative_interval = (path_seg_cumulative_length, path_seg_cumulative_length+seg_end-seg_start)
-            path_seg_cumulative_length += seg_end - seg_start
-            if re.match(CHROM_PATTERN, seg_chrom):
-                for del_block in del_blocks:
-                    # Both ends in the path segment
-                    if path_seg_cumulative_interval[0] <= del_block[1] and del_block[2] <= path_seg_cumulative_interval[1]:
-                        # convert them into exact coordinate
-                        # by adding segment starting coordinate and deletion block relative coordinate
-                        # TODO: confirm with Heng whether we need to consider partial match
-                        output_exact_coordiantes.append((seg_chrom, seg_start+del_block[1], seg_start+del_block[2], self.strand))
-        return output_exact_coordiantes
-
-    def extract_linear_reference_coordinate(self, parts: list, min_del_len: int = 50) -> list:
-        """ extract linear reference coordiante """
-        ref_pos = self.path_start
-        del_blocks = []
-        for length, op in parts:
-            length = int(length)
-            if op == 'D' and length >= min_del_len:
-                # exclude alternative contigs
-                if re.match(CHROM_PATTERN, self.path):
-                    del_blocks.append((self.path, ref_pos, ref_pos + length, self.strand))
-            if op in ["M", "D", "N", "=", "X"]:
-                ref_pos += length
-        return del_blocks
-
-    def is_skipped(self):
-        # 1. low mapping quality then return zero deletion block
-        # 2. no major reference contig then return zero deletion block
-        #    this only applies to GAF instead of PAF
-        return (self.mapq < 30) or ('chr' not in self.path)
-
-    def get_deletion_blocks(self, min_del_len: int =50) -> list:
-        """Identify large deletions from the CIGAR in gaf file
-        """
-        assert len(self.read) > 12, 'incomplete gaf'
-        if self.is_skipped():
-            return []
-
-        parts = CIGAR_REGEX.findall(self.cigar)
-        # Test the path length 
-        assert sum([int(t[0]) for t in parts if t[1] in ["M", "D", "N", "=", "X"]]) == self.path_end - self.path_start
-        #print(self.read[6])
-        #print(self.path_end - self.path_start)
-        # linear genome coordinate from minimap2
-        # or linear reference segment coordinate from minigraph
-        if ('>' not in self.path) and ('<' not in self.path):
-            del_blocks = self.extract_linear_reference_coordinate(parts)
-        else:
-            del_blocks = self.extract_graph_path_exact_coordinate(parts)
-        return del_blocks
-
     def convert_type(self):
         # convert element types
         for i in range(1, 4):
@@ -332,6 +232,7 @@ class AlignedRead(object):
                 # [path, absolute path start, absolute path end, path orientation, relative seg start, relative seg end]
                 seg.append([m[1], st, en, 1 if m[0] == '>' else -1,
                             y, y + (en - st)])
+                # accumulate seg start
                 y += en - st
         else:
             # https://github.com/lh3/miniasm/blob/master/PAF.md
@@ -422,7 +323,7 @@ class AlignedRead(object):
 
                 # relative seg start
                 off = seg[sts[i][0]][4]
-                # ask Heng, why always "+"
+                # minigraph always use "+" in genome path 
                 # convert back to relative path coordinate
                 output_indels.append(["".join(path), a[i][0] - off, a[i][1] - off, self.read[0], self.mapq, "+", a[i][2]])
         return output_indels
